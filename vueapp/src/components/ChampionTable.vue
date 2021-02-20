92b6258957482197cb033abcaafe98282bbcc968
Dylanelley <dylanelley@hotmail.com>
2021-02-20 21:52:15 +1300

    added table component

---
+++ vueapp/src/components/ChampionTable.vue
@@ -0,0 +1,19 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Champion</th>
+                <th>Games Played</th>
+                <th>Average KDA</th>
+                <th>Max Kills</th>
+                <th>Penta Kills</th>
+            </tr>
+        </thead>
+    </table>
+</template>
+
+<script>
+export default {
+    name: "ChampionTable",
+}
+</script>
